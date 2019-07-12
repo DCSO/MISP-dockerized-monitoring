4247e18cd0808ecaa25595453d1899b38895957b
8ear <github@8ear.de>
2019-07-12 17:59:17 +0200

    Add v1.16

---
+++ v1.16.0/files/entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -eu
+
+# shellcheck disable=2039
+jobs_httpcheck="
+- name: https://${MISP_FQDN}
+  url: https://${MISP_FQDN}
+  not_follow_redirects: no
+  tls_skip_verify: yes
+"
+
+jobs_mysql="
+- name: MISP-DB
+  dsn: $MYSQL_USER:$MYSQL_PASSWORD@tcp($MYSQL_HOST:$MYSQL_PORT)/$MYSQL_DATABASE?charset=utf8
+"
+
+jobs_redis="
+misp-redis:
+    name     : misp-redis
+    host     : ${REDIS_FQDN}
+    port     : ${REDIS_PORT}
+"
+
+# shellcheck disable=2039
+jobs_x509="
+  - name: https://${MISP_FQDN}
+    source: https://${MISP_FQDN}
+"
+
+# First check if job exists, if not add it
+! grep -q "$jobs_httpcheck" /etc/netdata/go.d/httpcheck.conf && echo "$jobs_httpcheck" >> /etc/netdata/go.d/httpcheck.conf
+! grep -q "$jobs_mysql" /etc/netdata/go.d/mysql.conf && echo "$jobs_mysql" >> /etc/netdata/go.d/mysql.conf
+! grep -q "$jobs_redis" /etc/netdata/python.d/redis.conf && echo "$jobs_redis" >> /etc/netdata/python.d/redis.conf
+! grep -q "$jobs_x509" /etc/netdata/go.d/x509check.conf && echo "$jobs_x509" >> /etc/netdata/go.d/x509check.conf
+
+
+# Start Default Netdata Entrypoint
+exec /usr/sbin/run.sh
